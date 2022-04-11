61680109494cb0006266763d628030d82dc01b37
giangthao <thaogiang.90@gmail.com>
2022-04-11 13:04:47 +0700

    14. Exploring Page Object WebElementFacade Interface

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,6 +1,8 @@
 package testcases;
 
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Title;
@@ -8,21 +10,31 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 @RunWith(SerenityRunner.class)
 public class LoginTest extends PageObject {
+    @FindBy(name="q")
+    WebElement googleSearch;
     @Managed(driver = "chrome")
     WebDriver driver;
     @Title("Executing Login Test")
     @Test
-    public void doLogin(){
+    public void doLogin() throws InterruptedException {
         //Lession 10
         //driver.get("https://google.com");
         open();
-        driver.findElement(By.name("q")).sendKeys("Hello Serenity !");
+       /* driver.findElement(By.name("q")).sendKeys("Hello Serenity !");
         find(By.xpath("//*[@name='q']")).clear();
         find(By.xpath("//*[@name='q']")).clear();
         find(By.xpath("//*[@name='q']")).sendKeys("Using Find Serenity!");
-        typeInto($("//*[@name='q']"),"Hello Try into");
+        typeInto($("//*[@name='q']"),"Hello Try into");*/
         System.out.println(getTitle());
+        WebElementFacade textBox = find(By.name("q"));
+        textBox.shouldBePresent();
+        textBox.type("Hello Selenium !!!");
+        textBox.typeAndEnter("Hello Selenium!!!");
+        Thread.sleep(2000);
+
     }
 }
