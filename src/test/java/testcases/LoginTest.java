1780067434ee85d3fc817f0fe172050dd17701d2
giangthao <thaogiang.90@gmail.com>
2022-04-11 12:21:06 +0700

    13. Exploring Page Object

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,19 +1,28 @@
 package testcases;
 
+import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Title;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 @RunWith(SerenityRunner.class)
-public class LoginTest {
-    @Managed
+public class LoginTest extends PageObject {
+    @Managed(driver = "chrome")
     WebDriver driver;
+    @Title("Executing Login Test")
     @Test
     public void doLogin(){
         //Lession 10
-        driver.get("https://google.com");
+        //driver.get("https://google.com");
+        open();
         driver.findElement(By.name("q")).sendKeys("Hello Serenity !");
+        find(By.xpath("//*[@name='q']")).clear();
+        find(By.xpath("//*[@name='q']")).clear();
+        find(By.xpath("//*[@name='q']")).sendKeys("Using Find Serenity!");
+        typeInto($("//*[@name='q']"),"Hello Try into");
+        System.out.println(getTitle());
     }
 }
